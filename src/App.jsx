b92c700e7092ef17d4c16d1adb20d07bf30a9398
Dmitry <samoulovdima@mail.ru>
2024-07-19 02:53:01 +0500

    +main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./components.css";
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (buttonId) => {
+    if (activeButton === buttonId){
+      setActiveButton(null);
+    }
+    else setActiveButton(buttonId);
+  };
+
+  const handleStartClick = () => {
+    if (activeButton === null) {
+      const spanElement = document.getElementById('Choose');
+      spanElement.classList.add('shake');
+
+      setTimeout(() => {
+        spanElement.classList.remove('shake');
+      }, 1000);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <link rel="icon" href="data:;base64,="></link>
+      <div className="grid-container">
+        <div className="column column1">
+          <div className="row1">
+            <div className="container-row1">
+              <span id="Choose" className="text">Choose</span>
+              <button className={`btn-quiz ${activeButton === 'btn-quiz1' ? 'clicked' : 'unclicked'}`}
+                      onClick={() => handleClick('btn-quiz1')}>
+                Trivia
+              </button>
+              <button className={`btn-quiz ${activeButton === 'btn-quiz2' ? 'clicked' : 'unclicked'}`}
+                      onClick={() => handleClick('btn-quiz2')}>
+                Math
+              </button>
+              <button className={`btn-quiz ${activeButton === 'btn-quiz3' ? 'clicked' : 'unclicked'}`}
+                      onClick={() => handleClick('btn-quiz3')}>
+                Date
+              </button>
+              <button className={`btn-start ${activeButton === null ? '' : 'clicked'}`}
+                      onClick={handleStartClick}>
+                Start
+              </button>
+            </div>
+          </div>
+          <div className="row2">
+            <div className="container-row2">
+              <span className="text">Your stats</span>
+              <div className="container-stats">
+                <span className="text-stats">Correct: 0</span>
+                <span className="text-stats">Incorrect: 0</span>
+                <span className="text-stats">Hints used: 0</span>
+                <span className="text-stats">AVG: 0</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column column2">
+          <div className="content">
+            <img src="/svg_quiz.svg" alt='QUIZ'/>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
